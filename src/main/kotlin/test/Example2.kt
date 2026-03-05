072452f7103d8de261052b4a6b783c68f13bc119
kerikir <vlasov-kerik@mail.ru>
2026-03-05 20:19:12 +0300

    added the solution to the second task

--- src/main/kotlin/test/Example2.kt
+++ src/main/kotlin/test/Example2.kt
@@ -1,5 +1,34 @@
 package test
 
 fun main() {
+    val n = readln().toInt()
+    val capitalization = readln().trim().split(" ").map { it.toInt() }
 
+    val mergerArray = IntArray(n)
+    mergerArray[0] = capitalization[0]
+    for (i in 1 until n) {
+        mergerArray[i] = capitalization[i] + mergerArray[i - 1]
+    }
+
+    val result = mutableListOf<Int>()
+    if (capitalization[capitalization.lastIndex] > capitalization[capitalization.lastIndex - 1]) {
+        result.add(1)
+    } else {
+        val numberOfCapitalizationLessMax = capitalization.count { it < capitalization[capitalization.lastIndex] }
+        if (numberOfCapitalizationLessMax > 0) {
+            result.add(1)
+        } else {
+            result.add(0)
+        }
+    }
+
+    for (i in (capitalization.lastIndex - 1) downTo 0) {
+        if (mergerArray[i] > capitalization[i + 1]) {
+            result.add(0, 1)
+        } else {
+            result.add(0, 0)
+        }
+    }
+
+    println(result.joinToString("\n"))
 }
