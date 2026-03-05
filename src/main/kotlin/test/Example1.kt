2804bf32c74eb99dbeff7e9feddc6c922da4fbc2
kerikir <vlasov-kerik@mail.ru>
2026-03-05 19:50:02 +0300

    added the solution to the first task

--- src/main/kotlin/test/Example1.kt
+++ src/main/kotlin/test/Example1.kt
@@ -1,5 +1,26 @@
 package test
 
 fun main() {
+    val msg = readln()
 
+    val reverseResult = mutableListOf<Char>()
+    var i = msg.length - 1
+
+    while (i >= 0) {
+
+        if (msg[i] == '#') {
+            val number = msg.subSequence(i - 2, i).toString().toInt()
+            val char = (97 + number - 1).toChar()
+            reverseResult.add(char)
+            i -= 3
+
+        } else {
+            val number = msg[i].digitToInt()
+            val char = (97 + number - 1).toChar()
+            reverseResult.add(char)
+            i--
+        }
+    }
+
+    println(reverseResult.reversed().joinToString(""))
 }
