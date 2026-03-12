b76efcc5764e0b224c86796b333db7e6cba486cd
kerikir <vlasov-kerik@mail.ru>
2026-03-12 21:22:33 +0300

    added a definition of the number of colored trees

---
+++ src/main/kotlin/lesson1/complexityOfAlgorithms/PaintingTrees.kt
@@ -0,0 +1,25 @@
+package lesson1.complexityOfAlgorithms
+
+fun main() {
+    val (p, v) = readln().trim().split(Regex("\\s+")).map { it.toInt() }
+    val (q, m) = readln().trim().split(Regex("\\s+")).map { it.toInt() }
+
+    val x1 = p - v
+    val x2 = p + v
+    val y1 = q - m
+    val y2 = q + m
+
+    var doublePaint = 0
+    if (x1 <= y1 && y2 <= x2) {
+        doublePaint = y2 - y1 + 1
+    } else if (y1 <= x1 && x2 <= y2) {
+        doublePaint = x2 - x1 + 1
+    } else if (x2 >= y1 && p < q) {
+        doublePaint = x2 - y1 + 1
+    } else if (y2 >= x1 && q < p) {
+        doublePaint = y2 - x1 + 1
+    }
+
+    val result = (x2 - x1 + 1) + (y2 - y1 + 1) - doublePaint
+    println(result)
+}
