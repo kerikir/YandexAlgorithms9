5f006c6ec130835384be687453d8ba17b159e741
kerikir <vlasov-kerik@mail.ru>
2026-03-14 21:56:12 +0300

    added a definition of the number of pairs of clothes to pull out

--- src/main/kotlin/lesson1/testAutomation/ClothingColors.kt
+++ src/main/kotlin/lesson1/testAutomation/ClothingColors.kt
@@ -28,6 +28,26 @@ fun main() {
 
 fun calculationNumberOfPairs(a: Int, b: Int, c: Int, d: Int): Pair<Int, Int> {
 
+    if (a == 0 && c != 0) {
+        return b to d + 1
+    }
+    if (a != 0 && c == 0) {
+        return b + 1 to d
+    }
+    if (a == 0 && c == 0) {
+        return b to d
+    }
+
+    if (b == 0 && d == 0) {
+        return a to c
+    }
+    if (b == 0 && d != 0) {
+        return a to (c + 1)
+    }
+    if (b != 0 && d == 0) {
+        return (a + 1) to c
+    }
+
     return if (a + c < b + d) {
         (a + 1) to (c + 1)
     } else {
