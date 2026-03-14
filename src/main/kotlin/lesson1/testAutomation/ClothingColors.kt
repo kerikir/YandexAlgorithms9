1c49e732e1b5a67686a3bd1e9ff30067f58de69c
kerikir <vlasov-kerik@mail.ru>
2026-03-14 22:06:59 +0300

    fixed a bug when there are no items of a certain color

--- src/main/kotlin/lesson1/testAutomation/ClothingColors.kt
+++ src/main/kotlin/lesson1/testAutomation/ClothingColors.kt
@@ -5,7 +5,7 @@ package lesson1.testAutomation
 Задача: Пара одноцветной одежды - найти минимальное число вытягиваний для комплекта в одном цвете
 
 Сложность = O(1)
-Ограничения: 1 <= N <= 1_000_000_000
+Ограничения: 0 <= A, B, C, D <= 1_000_000_000
 
 Время = 101ms
 Память = 19.85MB
@@ -29,23 +29,42 @@ fun main() {
 fun calculationNumberOfPairs(a: Int, b: Int, c: Int, d: Int): Pair<Int, Int> {
 
     if (a == 0 && c != 0) {
-        return 1 to c + 1
+        return if (c <= d) {
+            1 to c + 1
+        } else {
+            1 to d + 1
+        }
     }
+
     if (a != 0 && c == 0) {
-        return a + 1 to 1
+        return if (a <= b) {
+            a + 1 to 1
+        } else {
+            b + 1 to 1
+        }
     }
+
     if (a == 0 && c == 0) {
         return 1 to 1
     }
-
     if (b == 0 && d == 0) {
         return 1 to 1
     }
+
     if (b == 0 && d != 0) {
-        return 1 to (d + 1)
+        return if (c < d) {
+            1 to c + 1
+        } else {
+            1 to d + 1
+        }
     }
+
     if (b != 0 && d == 0) {
-        return (b + 1) to 1
+        return if (a < b) {
+            a + 1 to 1
+        } else {
+            b + 1 to 1
+        }
     }
 
     return if (a + c < b + d) {
