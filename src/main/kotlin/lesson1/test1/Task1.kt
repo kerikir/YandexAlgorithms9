76fd023648bbe089b95c340a46ab766ff5e4beeb
kerikir <vlasov-kerik@mail.ru>
2026-03-17 19:12:43 +0300

    added a solution to the first problem

--- src/main/kotlin/lesson1/test1/Task1.kt
+++ src/main/kotlin/lesson1/test1/Task1.kt
@@ -2,4 +2,27 @@ package lesson1.test1
 
 fun main() {
 
+    val (n, m) = readln().trim().split(Regex("\\s+")).map { it.toInt() }
+
+    val charArray = Array<CharArray>(n) {
+        readln().trim().toCharArray()
+    }
+
+    var counter = 0
+
+    for (i in 0..<n) {
+        for (j in 0..<m) {
+            if (j != m - 1) {
+                if (charArray[i][j] == '.' && charArray[i][j + 1] == '.')
+                    counter++
+            }
+
+            if (i != n - 1) {
+                if (charArray[i][j] == '.' && charArray[i + 1][j] == '.')
+                    counter++
+            }
+        }
+    }
+
+    println(counter)
 }
