bc6b22332946649ae84fa167b16c476fb5c11f04
kerikir <vlasov-kerik@mail.ru>
2026-03-18 23:22:34 +0300

    added the comments

--- src/main/kotlin/lesson2/setWorking/IntersectionSets.kt
+++ src/main/kotlin/lesson2/setWorking/IntersectionSets.kt
@@ -1,5 +1,18 @@
 package lesson2.setWorking
 
+/*
+
+Задача: Общие числа - пересечение множеств
+
+Сложность = O(N)
+Ограничения: 1 <= N <= 10_000
+
+Время = 716ms
+Память = 43.27MB
+
+ */
+
+
 fun main() {
     val array1 = readln().trim().split(Regex("\\s+")).map { it.toInt() }
     val array2 = readln().trim().split(Regex("\\s+")).map { it.toInt() }
