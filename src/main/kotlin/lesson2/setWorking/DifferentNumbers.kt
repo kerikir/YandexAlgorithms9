7a33aa6c00f80326b3315de5f4e16b4f58f086f0
kerikir <vlasov-kerik@mail.ru>
2026-03-18 23:03:34 +0300

    added the comments

--- src/main/kotlin/lesson2/setWorking/DifferentNumbers.kt
+++ src/main/kotlin/lesson2/setWorking/DifferentNumbers.kt
@@ -1,5 +1,18 @@
 package lesson2.setWorking
 
+/*
+
+Задача: Разные числа - определить количество различных чисел
+
+Сложность = O(N)
+Ограничения: 0 <= N <= 100_000
+
+Время = 448ms
+Память = 37.02MB
+
+ */
+
+
 fun main() {
     val numbers = readln().trim().split(Regex("\\s+")).map { it.toInt() }
 
