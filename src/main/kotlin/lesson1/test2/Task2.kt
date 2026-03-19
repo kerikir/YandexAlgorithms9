dd3009b9e5aa32c62b618b423fc6a4c30d14a258
kerikir <vlasov-kerik@mail.ru>
2026-03-19 21:49:16 +0300

    added a solution to the second problem

--- src/main/kotlin/lesson1/test2/Task2.kt
+++ src/main/kotlin/lesson1/test2/Task2.kt
@@ -10,23 +10,19 @@ fun main() {
 
     while (!isCorrect(n, m, h, w)) {
 
-        if (n > h && n > w) {
-            n = ceil(n / 2.0).toLong()
-        } else if (m > h && m > w) {
-            m = ceil(m / 2.0).toLong()
-        } else if ((m > h && n > w) || (m > w && n > h)) {
-            if (m > n) {
-                m = ceil(m / 2.0).toLong()
-            } else {
-                n = ceil(n / 2.0).toLong()
-            }
-        } else if ((n > h && m <= w) || (n > w && m <= h)) {
-            n = ceil(n / 2.0).toLong()
-        } else if ((m > h && n <= w) || (m > w && n <= h)) {
-            m = ceil(m / 2.0).toLong()
+        if (h < n && h < m) {
+            h *= 2
+            counter++
+        } else if (w < n && w < m) {
+            w *= 2
+            counter++
+        } else if (h in m..<n) {
+            w *= 2
+            counter++
+        } else if (w in m..<n) {
+            counter++
+            h *= 2
         }
-
-        counter++
     }
 
     println(counter)
