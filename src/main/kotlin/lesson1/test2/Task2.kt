51dd5383a4e170f0fbfe180788d7a7eaffc18ab3
kerikir <vlasov-kerik@mail.ru>
2026-03-19 21:34:41 +0300

    added a solution to the second problem

--- src/main/kotlin/lesson1/test2/Task2.kt
+++ src/main/kotlin/lesson1/test2/Task2.kt
@@ -1,5 +1,38 @@
 package lesson1.test2
 
-fun main() {
+import kotlin.math.ceil
 
+
+fun main() {
+    var (n, m, h, w) = readln().trim().split(Regex("\\s+")).map { it.toLong() }
+
+    var counter = 0
+
+    while (!isCorrect(n, m, h, w)) {
+
+        if (n > h && n > w) {
+            n = ceil(n / 2.0).toLong()
+        } else if (m > h && m > w) {
+            m = ceil(m / 2.0).toLong()
+        } else if ((n > h && m <= w) || (n > w && m <= h)) {
+            n = ceil(n / 2.0).toLong()
+        } else if ((m > h && n <= w) || (m > w && n <= h)) {
+            m = ceil(m / 2.0).toLong()
+        } else if ((m > h && n > w) || (m > w && n > h)) {
+            if (m > n) {
+                m = ceil(m / 2.0).toLong()
+            } else {
+                n = ceil(n / 2.0).toLong()
+            }
+        }
+
+        counter++
+    }
+
+    println(counter)
+}
+
+
+fun isCorrect(n: Long, m: Long, h: Long, w: Long): Boolean {
+    return (n <= h && m <= w) || (n <= w && m <= h)
 }
