9c25caf074a18d953a0c40ab680608f7a7b3f1ac
kerikir <vlasov-kerik@mail.ru>
2026-03-19 21:12:38 +0300

    added a solution to the first problem

--- src/main/kotlin/lesson1/test2/Task1.kt
+++ src/main/kotlin/lesson1/test2/Task1.kt
@@ -1,5 +1,35 @@
 package lesson1.test2
 
 fun main() {
+    val n = readln().trim().toInt()
+    val msg = readln().trim()
 
+    var left = 0
+    var maxLength = 0
+    var currLength = 0
+
+    while (left < n) {
+
+        if (msg[left] == 'a' || msg[left] == 'h') {
+
+            if (left > 0 && msg[left] != msg[left - 1]) {
+                currLength++
+            } else if (left == 0) {
+                currLength++
+            } else {
+                currLength = 1
+            }
+
+            if (currLength > maxLength) {
+                maxLength = currLength
+            }
+
+        } else {
+            currLength = 0
+        }
+
+        left++
+    }
+
+    println(maxLength)
 }
