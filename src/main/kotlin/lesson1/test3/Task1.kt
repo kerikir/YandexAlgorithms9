a4310f116a4a80260d72b52b5ddb36d363ce8b81
kerikir <vlasov-kerik@mail.ru>
2026-03-21 11:06:18 +0300

    added a solution to the first problem

--- src/main/kotlin/lesson1/test3/Task1.kt
+++ src/main/kotlin/lesson1/test3/Task1.kt
@@ -1,5 +1,22 @@
 package lesson1.test3
 
 fun main() {
+    val str = readln().trim().split(Regex("\\s+"))
 
+    val result = mutableListOf<String>()
+    for (word in str) {
+        var letf = 0
+        var right = 0
+
+        while (word[letf] == '\'') {
+            letf++
+        }
+        while (word[word.length - 1 - right] == '\'') {
+            right++
+        }
+
+        result.add(word.substring(letf * 2, word.length - right * 2))
+    }
+
+    println(result.joinToString(""))
 }
