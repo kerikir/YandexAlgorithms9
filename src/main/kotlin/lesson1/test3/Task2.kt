b5b236af03dfbf157ec51ede030c986c9ac6c023
kerikir <vlasov-kerik@mail.ru>
2026-03-21 11:27:08 +0300

    added a solution to the second problem for the first three tests

--- src/main/kotlin/lesson1/test3/Task2.kt
+++ src/main/kotlin/lesson1/test3/Task2.kt
@@ -1,5 +1,30 @@
 package lesson1.test3
 
-fun main() {
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
 
+fun main() {
+    val n = readln().trim().toLong()
+
+    val minLength = floor(sqrt(n.toDouble())).toLong()
+    val maxLength = ceil(sqrt(n.toDouble())).toLong()
+
+    if (n == (maxLength * maxLength)) {
+        println(0)
+        return
+    }
+
+    var additionalPlaceOdd = maxLength / 2
+    var additionalPlaceEven = (maxLength + 1) / 2
+
+    if (n == (maxLength * maxLength - additionalPlaceOdd) || n == (maxLength * maxLength - additionalPlaceEven)) {
+        println(0)
+        return
+    }
+
+    if (n == minLength * maxLength) {
+        println(1)
+        return
+    }
 }
