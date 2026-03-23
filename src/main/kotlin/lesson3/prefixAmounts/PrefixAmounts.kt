92d7058a50e90a9c77ffa926396778c517592357
kerikir <vlasov-kerik@mail.ru>
2026-03-24 00:15:56 +0300

    added the comments

--- src/main/kotlin/lesson3/prefixAmounts/PrefixAmounts.kt
+++ src/main/kotlin/lesson3/prefixAmounts/PrefixAmounts.kt
@@ -1,5 +1,19 @@
 package lesson3.prefixAmounts
 
+/*
+
+Задача: Префиксные суммы - составить последовательность префиксных сумм для последовательности чисел
+
+Сложность = O(N)
+Ограничения: 1 <= N <= 1_000
+             -1_000_000 <= Ai <= 1_000_000
+
+Время = 190ms
+Память = 25.91MB
+
+ */
+
+
 fun main() {
     val n = readln().trim().toInt()
     val numbers = readln().trim().split(Regex("\\s+")).map { it.toInt() }
