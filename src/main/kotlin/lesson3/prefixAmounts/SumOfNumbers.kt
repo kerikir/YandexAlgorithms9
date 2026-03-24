0c548e2c6580c51beacc7ca6fb8d343bc6ebbfa9
kerikir <vlasov-kerik@mail.ru>
2026-03-24 21:01:28 +0300

    added the comments

--- src/main/kotlin/lesson3/prefixAmounts/SumOfNumbers.kt
+++ src/main/kotlin/lesson3/prefixAmounts/SumOfNumbers.kt
@@ -1,5 +1,20 @@
 package lesson3.prefixAmounts
 
+/*
+
+Задача: Сумма номеров - определение количества подряд идущих машин с необходимой суммой номеров
+
+Сложность = O(N)
+Ограничения: 1 <= N <= 100_000
+             1 <= K <= 1_000_000_000
+             1 <= Ai <= 999
+
+Время = 415ms
+Память = 34.21MB
+
+ */
+
+
 fun main() {
     val (n, k) = readln().trim().split(Regex("\\s+")).map { it.toInt() }
     val numbers = readln().trim().split(Regex("\\s+")).map { it.toInt() }
