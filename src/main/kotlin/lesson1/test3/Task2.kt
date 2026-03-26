66187f9688cf3f0d1fa16cbf5431393870084c4b
kerikir <vlasov-kerik@mail.ru>
2026-03-26 22:30:28 +0300

    added a solution to the problem of desks and a bunker

--- src/main/kotlin/lesson1/test3/Task2.kt
+++ src/main/kotlin/lesson1/test3/Task2.kt
@@ -1,54 +1,28 @@
 package lesson1.test3
 
-import kotlin.math.abs
-import kotlin.math.ceil
-import kotlin.math.floor
-import kotlin.math.sqrt
+import kotlin.math.*
 
 fun main() {
     val n = readln().trim().toLong()
 
-    val minLength = floor(sqrt(n.toDouble())).toLong()
-    val maxLength = ceil(sqrt(n.toDouble())).toLong()
-
-    if (n == (maxLength * maxLength)) {
-        println(0)
-        return
-    }
-
-    var additionalPlaceOdd = maxLength / 2
-    var additionalPlaceEven = (maxLength + 1) / 2
-
-    if (n == (maxLength * maxLength - additionalPlaceOdd) || n == (maxLength * maxLength - additionalPlaceEven)) {
-        println(0)
-        return
-    }
-
-    if (n == minLength * maxLength) {
-        println(1)
-        return
-    }
-
+    var columns = 1L
     var minDiff = n
-    for (i in 1..maxLength) {
 
-        for (j in i..(maxLength * 2)) {
-            val placeOdd = j / 2
-            val placeEven = (j + 1) / 2
+    while (columns * columns < 2 * n) {
 
-            if (j * i == n) {
-                val diff = abs(j - i)
-                if (diff < minDiff) minDiff = diff
-            }
-            if (j * i + placeOdd == n || j * i + placeEven == n) {
-                val diff = abs(j + 1 - i)
-                if (diff < minDiff) minDiff = diff
-            }
-            if (j * i - placeOdd == n || j * i - placeEven == n) {
-                val diff = abs(j - 1 - i)
-                if (diff < minDiff) minDiff = diff
+        if (n % columns == 0L) {
+            minDiff = min(minDiff, abs(columns - (n / columns)))
+
+        } else {
+            val minColumn = n / columns
+            val extra = n - columns * minColumn
+
+            if (columns / 2 == extra || (columns + 1) / 2 == extra) {
+                minDiff = min(minDiff, abs(columns - (minColumn + 1)))
             }
         }
+
+        columns++
     }
 
     println(minDiff)
