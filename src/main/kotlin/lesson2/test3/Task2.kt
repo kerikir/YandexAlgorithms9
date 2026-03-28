cf82a268cc95a0120c7298d873484525fdcdc058
kerikir <vlasov-kerik@mail.ru>
2026-03-28 12:13:55 +0300

    added a solution to the second problem

--- src/main/kotlin/lesson2/test3/Task2.kt
+++ src/main/kotlin/lesson2/test3/Task2.kt
@@ -6,7 +6,7 @@ fun main() {
 
     val ban = mutableSetOf<Int>()
     for (i in 0..<n) {
-        val rotate = (array[i] - i) % n
+        val rotate = (i - array[i] + 1) % n
         ban.add(rotate)
     }
 
