13f41a26aef9c5383750fb37d7cbcd3dcd8f0fe6
kerikir <vlasov-kerik@mail.ru>
2026-03-28 11:56:43 +0300

    added a solution to the second problem

--- src/main/kotlin/lesson2/test3/Task2.kt
+++ src/main/kotlin/lesson2/test3/Task2.kt
@@ -4,28 +4,18 @@ fun main() {
     val n = readln().trim().toInt()
     val array = readln().trim().split(Regex("\\s+")).map { it.toInt() }.toIntArray()
 
+    val ban = mutableSetOf<Int>()
+    for (i in 0..<n) {
+        val rotate = (array[i] - i) % n
+        ban.add(rotate)
+    }
+
     repeat(n) {
-        if (isCorrect(n, array, it)) {
+        if (it !in ban) {
             println(it)
             return
         }
     }
 
     println(-1)
-}
-
-
-fun isCorrect(n: Int, array: IntArray, shift: Int): Boolean {
-    var flag = true
-
-    for (i in 0..<n) {
-        val food = array[(i+ shift) % n]
-        flag = flag && (food != (i + 1))
-
-        if (!flag) {
-            return false
-        }
-    }
-
-    return flag
 }
