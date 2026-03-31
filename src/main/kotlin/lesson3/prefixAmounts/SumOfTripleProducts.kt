af09896eecef1fccf4559fc27dd44f11e10cdb36
kerikir <vlasov-kerik@mail.ru>
2026-03-31 17:23:39 +0300

    added definition of the sum of triple products

--- src/main/kotlin/lesson3/prefixAmounts/SumOfTripleProducts.kt
+++ src/main/kotlin/lesson3/prefixAmounts/SumOfTripleProducts.kt
@@ -4,21 +4,21 @@ fun main() {
     val n = readln().trim().toInt()
     val numbers = readln().trim().split(Regex("\\s+")).map { it.toInt() }
 
-    val mod = 1_000_000_007
+    val mod = 1_000_000_007L
 
-    val prefixSum = IntArray(n)
-    prefixSum[0] = numbers.first()
+    val prefixSum = LongArray(n)
+    prefixSum[0] = numbers.first().toLong()
     for (i in 1..<n) {
         prefixSum[i] = (prefixSum[i - 1] + numbers[i]) % mod
     }
 
-    val suffixSum = IntArray(n)
-    suffixSum[0] = numbers.last()
+    val suffixSum = LongArray(n)
+    suffixSum[0] = numbers.last().toLong()
     for (i in 1..<n) {
         suffixSum[i] = (suffixSum[i - 1] + numbers[n - 1 - i]) % mod
     }
 
-    var result = 0
+    var result = 0L
     for (i in 1..(n - 2)) {
         result = (result + prefixSum[i - 1] * numbers[i] * suffixSum[n - i - 2]) % mod
     }
