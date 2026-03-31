5e510de1bd55deec04a27361ef53784afe0c8913
kerikir <vlasov-kerik@mail.ru>
2026-03-31 19:34:05 +0300

    added a solution to the first problem

--- src/main/kotlin/lesson3/test1/Task1.kt
+++ src/main/kotlin/lesson3/test1/Task1.kt
@@ -1,5 +1,29 @@
 package lesson3.test1
 
 fun main() {
+    val n = readln().trim().toInt()
+    val string = readln().trim()
 
+    val pair = mutableMapOf<Int, Int>()
+
+    var sum = 0
+    pair[sum] = pair.getOrDefault(sum, 0) + 1
+
+    var counter = 0L
+
+    for (gender in string) {
+        if (gender == 'a') {
+            sum += 1
+        }
+        if (gender == 'b') {
+            sum -= 1
+        }
+
+        if (sum in pair) {
+            counter += pair.getOrDefault(sum, 0)
+        }
+        pair[sum] = pair.getOrDefault(sum, 0) + 1
+    }
+
+    println(counter)
 }
