974887e79c6a669417d420f6f16a5ea0c0f6a37e
kerikir <vlasov-kerik@mail.ru>
2026-03-31 19:17:57 +0300

    added a solution to the second problem

--- src/main/kotlin/lesson3/test1/Task2.kt
+++ src/main/kotlin/lesson3/test1/Task2.kt
@@ -1,5 +1,44 @@
 package lesson3.test1
 
 fun main() {
+    val n = readln().trim().toInt()
+    val sweets = readln().trim().split(Regex("\\s+")).map { it.toInt() }
 
+    val types = mutableMapOf<Int, Int>()
+
+    var currCount = 0
+    var maxCount = 0
+    var right = 0
+
+    for (left in 0..<n) {
+        while (right < n && types.size <= 2) {
+
+            if (types.size == 2 && sweets[right] !in types) {
+                currCount = right - left
+                if (maxCount < currCount) {
+                    maxCount = currCount
+                }
+                break
+
+            } else {
+                types[sweets[right]] = types.getOrDefault(sweets[right], 0) + 1
+                right++
+            }
+        }
+
+        if (right == n && types.size == 2) {
+            currCount = right - left
+            if (maxCount < currCount) {
+                maxCount = currCount
+            }
+        }
+
+        if (types.getOrDefault(sweets[left], 1) > 1) {
+            types[sweets[left]] = types.getOrDefault(sweets[left], 1) - 1
+        } else {
+            types.remove(sweets[left])
+        }
+    }
+
+    println(maxCount)
 }
