7affbd88c7c63f05a82436498dda7aa772e068ed
kerikir <vlasov-kerik@mail.ru>
2026-04-01 20:25:43 +0300

    added an error-proof stack solution

---
+++ src/main/kotlin/lesson4/functionLaunchMechanism/ErrorProofStack.kt
@@ -0,0 +1,52 @@
+package lesson4.functionLaunchMechanism
+
+import java.util.Stack
+
+fun main() {
+
+    val stack = Stack<Int>()
+
+    while (true) {
+        val operation = readln().trim()
+
+        when {
+            operation.contains("push") -> {
+                val (_, n) = operation.split(Regex("\\s+"))
+                stack.push(n.toInt())
+                println("ok")
+            }
+
+            operation.contains("pop") -> {
+                if (stack.size > 0) {
+                    val a = stack.pop()
+                    println(a)
+                } else {
+                    println("error")
+                }
+            }
+
+            operation.contains("back") -> {
+                if (stack.size > 0) {
+                    val a = stack.peek()
+                    println(a)
+                } else {
+                    println("error")
+                }
+            }
+
+            operation.contains("size") -> {
+                println(stack.size)
+            }
+
+            operation.contains("clear") -> {
+                stack.clear()
+                println("ok")
+            }
+
+            operation.contains("exit") -> {
+                println("bye")
+                break
+            }
+        }
+    }
+}
