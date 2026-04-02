ab2c8f7a4565d8651910a7a4e0bf09bc7608072d
kerikir <vlasov-kerik@mail.ru>
2026-04-02 21:05:36 +0300

    added a solution to the first problem

--- src/main/kotlin/lesson3/test2/Task1.kt
+++ src/main/kotlin/lesson3/test2/Task1.kt
@@ -1,5 +1,37 @@
 package lesson3.test2
 
 fun main() {
-    val n = readln().trim().toInt()
+    val commands = readln().trim()
+
+    val points = mutableMapOf<Pair<Int, Int>, Int>()
+
+    var x = 0
+    var y = 0
+    points[0 to 0] = 1
+
+    for (command in commands) {
+        when(command) {
+            'U' -> {
+                y++
+                points[x to y] = points.getOrDefault(x to y, 0) + 1
+            }
+
+            'D' -> {
+                y--
+                points[x to y] = points.getOrDefault(x to y, 0) + 1
+            }
+
+            'R' -> {
+                x++
+                points[x to y] = points.getOrDefault(x to y, 0) + 1
+            }
+
+            'L' -> {
+                x--
+                points[x to y] = points.getOrDefault(x to y, 0) + 1
+            }
+        }
+    }
+
+    points.count { it.value > 1 }.also(::println)
 }
