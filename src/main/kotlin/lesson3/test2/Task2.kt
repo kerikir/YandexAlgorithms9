114cb80e79dcd23785768eb14d6afd04d5f6ed49
kerikir <vlasov-kerik@mail.ru>
2026-04-02 21:23:00 +0300

    added a solution to the second problem

--- src/main/kotlin/lesson3/test2/Task2.kt
+++ src/main/kotlin/lesson3/test2/Task2.kt
@@ -1,5 +1,29 @@
 package lesson3.test2
 
 fun main() {
-    val n = readln().trim().toInt()
+    val cases = readln().trim().toInt()
+
+    repeat(cases) {
+        val (n, d) = readln().trim().split(Regex("\\s+")).map { it.toInt() }
+        val t = IntArray(n)
+        val k = IntArray(n)
+        repeat(n) {
+            val (ti, ki) = readln().trim().split(Regex("\\s+")).map { it.toInt() }
+            t[it] = ti
+            k[it] = ki
+        }
+
+        val prefixSumWait = LongArray(n)
+        prefixSumWait[0] = d.toLong()
+        for (i in 1..<n) {
+            prefixSumWait[i] = prefixSumWait[i - 1] + k[i - 1]
+        }
+
+        var place = n + 1
+        while (place > 1 && prefixSumWait[place - 2] <= t[place - 2]) {
+            place--
+        }
+
+        println(place)
+    }
 }
