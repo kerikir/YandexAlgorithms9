2545b5ef90e18f5a5a1b0e3c937781c30cfc21e2
kerikir <vlasov-kerik@mail.ru>
2026-04-04 11:12:42 +0300

    added a solution to the first problem

--- src/main/kotlin/lesson3/test3/Task1.kt
+++ src/main/kotlin/lesson3/test3/Task1.kt
@@ -1,5 +1,27 @@
 package lesson3.test3
 
 fun main() {
-    val n = readln().trim().toInt()
+    val string = readln().trim()
+
+    val evaluations = mutableMapOf<Char, Int>()
+    for (char in string) {
+        evaluations[char] = evaluations.getOrDefault(char, 0) + 1
+    }
+
+    val charMin = string.max()
+
+    var sum = 0
+    var counter = 0
+    for (score in evaluations) {
+        sum += score.key.code * score.value
+        counter += score.value
+    }
+
+    var calculateScore = sum / counter
+
+    if (calculateScore >= charMin.code - 1) {
+        println(calculateScore.toChar())
+    } else {
+        println((charMin.code - 1).toChar())
+    }
 }
