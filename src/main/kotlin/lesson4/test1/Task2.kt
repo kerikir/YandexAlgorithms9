db3806c0747788423fcc4888869c6f07e67ceeef
kerikir <vlasov-kerik@mail.ru>
2026-04-07 19:52:21 +0300

    added a solution to the second problem

--- src/main/kotlin/lesson4/test1/Task2.kt
+++ src/main/kotlin/lesson4/test1/Task2.kt
@@ -1,5 +1,59 @@
 package lesson4.test1
 
+import java.util.Stack
+
 fun main() {
-    val n = readln().trim()
+    val (n, m, k) = readln().trim().split(Regex("\\s+")).map { it.toInt() }
+
+    val stacks = Array<ArrayDeque<String>>(n) {
+        ArrayDeque<String>()
+    }
+
+    val commands = mutableListOf<String>().apply {
+        repeat(m) {
+            val command = readln().trim()
+            add(command)
+        }
+    }
+
+    var page = 0
+    var buffer = emptyList<String>()
+
+    for (command in commands) {
+        when (command) {
+            "Next" -> { page = (page + 1) % n }
+
+            "Copy" -> {
+                buffer = stacks[page].toList()
+            }
+
+            "Paste" -> {
+                for (element in buffer) {
+                    stacks[page].addLast(element)
+                }
+                while (stacks[page].size > k) {
+                    stacks[page].removeFirst()
+                }
+            }
+
+            "Backspace" ->{
+                if (stacks[page].isNotEmpty()) {
+                    stacks[page].removeLast()
+                }
+            }
+
+            else -> {
+                stacks[page].addLast(command)
+                while (stacks[page].size > k) {
+                    stacks[page].removeFirst()
+                }
+            }
+        }
+    }
+
+    if (stacks[page].isNotEmpty()) {
+        println(stacks[page].joinToString(""))
+    } else {
+        println("Empty")
+    }
 }
