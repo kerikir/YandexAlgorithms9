87967b97310ed99fb87415e6e10a78ad816b1b61
kerikir <vlasov-kerik@mail.ru>
2026-04-08 18:04:07 +0300

    added a solution to the problem Histogram and rectangle

--- src/main/kotlin/lesson4/stackOfRecords/Histogram.kt
+++ src/main/kotlin/lesson4/stackOfRecords/Histogram.kt
@@ -1,41 +1,47 @@
 package lesson4.stackOfRecords
 
 import java.util.Stack
+import kotlin.math.max
 
 fun main() {
     val heights = readln().trim().split(Regex("\\s+")).map { it.toInt() }
 
     val stack = Stack<Pair<Int, Int>>()
 
-    val histograms = LongArray(heights.size)
+    val recordsStraight = IntArray(heights.size)
+    val recordsReverse = IntArray(heights.size)
 
     for (i in heights.indices) {
 
         while (stack.isNotEmpty() && heights[i] < stack.peek().second) {
             val (index, height) = stack.pop()
-            histograms[index] += height.toLong() * (i - index)
+            recordsStraight[index] = i
         }
         stack.push(i to heights[i])
     }
 
     while (stack.isNotEmpty()) {
         val (index, height) = stack.pop()
-        histograms[index] += height.toLong() * (heights.size - index)
+        recordsStraight[index] = heights.size
     }
 
     for (i in heights.indices.reversed()) {
 
         while (stack.isNotEmpty() && heights[i] < stack.peek().second) {
             val (index, height) = stack.pop()
-            histograms[index] += height.toLong() * (index - i - 1)
+            recordsReverse[index] = i
         }
         stack.push(i to heights[i])
     }
 
     while (stack.isNotEmpty()) {
         val (index, height) = stack.pop()
-        histograms[index] += height.toLong() * index
+        recordsReverse[index] = -1
     }
 
-    histograms.max().also(::println)
+    var maxArea = 0L
+    for ((index, height) in heights.withIndex()) {
+        maxArea = max(maxArea, height * (recordsStraight[index] - recordsReverse[index] - 1).toLong())
+    }
+    println(maxArea)
 }
