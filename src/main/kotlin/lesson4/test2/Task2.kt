c8665ea9484bdb2aee14d24e8113e969c9802d95
kerikir <vlasov-kerik@mail.ru>
2026-04-09 21:33:44 +0300

    added a solution to the second problem

--- src/main/kotlin/lesson4/test2/Task2.kt
+++ src/main/kotlin/lesson4/test2/Task2.kt
@@ -2,4 +2,54 @@ package lesson4.test2
 
 fun main() {
     val n = readln().trim().toInt()
+    val correctAnswers = readln().trim()
+    val m = readln().trim().toInt()
+    val answers = mutableListOf<String>().apply {
+        repeat(m) {
+            val ans = readln().trim()
+            add(ans)
+        }
+    }
+
+    var result = mutableListOf<Pair<Int, Int>>()
+
+    for (i in 0..<(m - 1)) {
+        for (j in (i + 1)..<m) {
+            if (isSimilarWorks(correctAnswers, answers[i], answers[j])) {
+                result.add(i + 1 to j + 1)
+            }
+        }
+    }
+
+    println(result.size)
+    result.forEach {
+        println("${it.first} ${it.second}")
+    }
+}
+
+
+fun isSimilarWorks(correctWork: String, work1: String, work2: String): Boolean {
+
+    var correctAnswers = 0
+    var incorrectAnswers = 0
+    var matchCorrectAnswers = 0
+    var matchInCorrectAnswers = 0
+
+    for (i in correctWork.indices) {
+        if (correctWork[i] == work1[i]) {
+            correctAnswers++
+            if (work1[i] == work2[i]) {
+                matchCorrectAnswers++
+            }
+
+        } else {
+            incorrectAnswers++
+            if (work1[i] == work2[i]) {
+                matchInCorrectAnswers++
+            }
+        }
+    }
+
+    return (matchCorrectAnswers / correctAnswers.toDouble()) > 0.5 &&
+            (matchInCorrectAnswers / incorrectAnswers.toDouble()) > 0.5
 }
