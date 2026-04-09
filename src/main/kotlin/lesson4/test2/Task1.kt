abb7afe795dbee50693b5908f237d5926877ede6
kerikir <vlasov-kerik@mail.ru>
2026-04-09 21:09:14 +0300

    added a solution to the first problem

--- src/main/kotlin/lesson4/test2/Task1.kt
+++ src/main/kotlin/lesson4/test2/Task1.kt
@@ -1,5 +1,61 @@
 package lesson4.test2
 
+import java.util.*
+
 fun main() {
-    val n = readln().trim().toInt()
+    val string = readln().trim()
+
+    var result = false
+    for (shift in string.indices) {
+        if (isCorrectParenthesisSequence(string, shift)) {
+            result = true
+        }
+    }
+
+    if (result) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
+
+
+fun isCorrectParenthesisSequence(string: String, k: Int): Boolean {
+    val stack = Stack<Char>()
+
+    for (index in string.indices) {
+
+        when (val parenthesis = string[(index + k) % string.length]) {
+
+            '(', '[', '{' -> {
+                stack.push(parenthesis)
+            }
+
+            ')' -> {
+                if (stack.isNotEmpty() && stack.peek() == '(') {
+                    stack.pop()
+                } else {
+                    return false
+                }
+            }
+
+            ']' -> {
+                if (stack.isNotEmpty() && stack.peek() == '[') {
+                    stack.pop()
+                } else {
+                    return false
+                }
+            }
+
+            '}' -> {
+                if (stack.isNotEmpty() && stack.peek() == '{') {
+                    stack.pop()
+                } else {
+                    return false
+                }
+            }
+        }
+    }
+
+    return stack.empty()
 }
