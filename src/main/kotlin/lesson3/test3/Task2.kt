b484e0ad72b4b0abd3933b6b1a3b629961141711
kerikir <vlasov-kerik@mail.ru>
2026-04-30 16:20:56 +0300

    added a solution to the problem of maximizing the value of prizes

--- src/main/kotlin/lesson3/test3/Task2.kt
+++ src/main/kotlin/lesson3/test3/Task2.kt
@@ -7,23 +7,27 @@ fun main() {
     val (n, k) = readln().trim().split(Regex("\\s+")).map { it.toInt() }
     val price = readln().trim().split(Regex("\\s+")).map { it.toInt() }
 
-    val prefixSum = LongArray(n)
-    prefixSum[0] = price.first().toLong()
-    for (i in 1..<n) {
+    val prefixSum = LongArray(n + 1)
+    prefixSum[0] = 0L
+    for (i in 1..n) {
         prefixSum[i] = prefixSum[i - 1] + price[i]
     }
 
-    var minArray = mutableListOf<Long>(0)
+    var minArray = mutableListOf<Long>(0L)
     var max = 0L
 
-    for (i in prefixSum.indices) {
+    for (i in 1..prefixSum.lastIndex) {
 
         minArray.add(prefixSum[i])
-
         minArray.sort()
-        if (minArray.size > 2 && minArray[1] % k == minArray.first() % k) {
-            minArray.removeAt(1)
+
+        for (j in minArray.lastIndex until 0) {
+            if (minArray[j] % k == minArray[j - 1] % k) {
+                minArray.removeAt(j)
+                break
+            }
         }
+
         minArray = minArray.take(2).toMutableList()
 
         max = if (minArray.first() % k == prefixSum[i] % k) {
